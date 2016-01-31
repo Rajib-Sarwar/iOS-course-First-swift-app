6b2282450d21fad04bdae9a8c406a2519e26262f
Ayon Chowdhury <md.rajib.sarwar@gmail.com>
2016-01-31 22:11:28 +0800

    Added files for my first app

--- FirstApp/ViewController.swift
+++ FirstApp/ViewController.swift
@@ -10,14 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var blueAppleImageView: UIImageView!
+    @IBOutlet weak var redAppleImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        blueAppleImageView.hidden = false;
+        redAppleImageView.hidden = false;
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func blueButtonPressed(sender: AnyObject) {
+        blueAppleImageView.hidden = true;
+    }
+    
+    @IBAction func redButtonPressed(sender: AnyObject) {
+        redAppleImageView.hidden = true;
     }
 
 
